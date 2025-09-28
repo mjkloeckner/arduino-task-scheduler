74bb6bde384e340714c1fe46f2fc973b1c34d856
Martin Kloeckner <mjkloeckner@gmail.com>
2025-09-28 00:41:47 -0300

    `OCR0A` set to 249 in order to have 250 ticks

--- src/millis.c
+++ src/millis.c
@@ -15,7 +15,7 @@ void millis_init(void) {
     TCCR0A = (1 << WGM01) | (1 << COM0A0);
     TCCR0B = (1 << CS01) | (1 << CS00);
     TIMSK0 = (1 << OCIE0A);
-    OCR0A = 250;
+    OCR0A = 249;
     sei();
 }
 
